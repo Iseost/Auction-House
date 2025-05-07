f623efc2eadd0f7a6d4fd56f29d844df63dc37c1
Iselin Engen <iselin.osterhus@gmail.com>
2025-05-07 13:39:43 +0200

    Styling feed
    The styling on the card is getting better

--- feed/feed.mjs
+++ feed/feed.mjs
@@ -11,36 +11,40 @@ export function createPostBox(coverImage, postTitle, postBody, id) {
     const content = document.getElementById("post_container");
 
     const card = document.createElement("div");
-    applyTailwindClasses(card, "flex flex-col m-4 p-4 rounded-lg bg-white cursor-pointer max-w-sm border border-gray-300 px-4 py-2");
+    applyTailwindClasses(card, "flex flex-col m-4 p-4 rounded-xl bg-white border border-darkFaded hover:shadow-xl transition-shadow duration-300 cursor-pointer max-w-sm overflow-hidden");
+
+    const imageContainer = document.createElement("div");
+    applyTailwindClasses(imageContainer, "flex items-center justify-center w-full max-h-[200px] sm:max-h-[240px] h-full cursor-pointer overflow-hidden drop-shadow-darkFaded");
 
     const postImage = document.createElement("img");
     postImage.src = coverImage || "./src/assets/image.png";
     postImage.alt = "Post Image";
-    applyTailwindClasses(postImage, "w-full h-auto rounded-md");
+    applyTailwindClasses(postImage, "object-cover w-full h-full hover:scale-110 transition-transform duration-300");
     postImage.addEventListener("click", () => {
         window.location.href = `./feed/post.html?postId=${id}`;
-
     });
+
+    imageContainer.appendChild(postImage);
 
     const title = document.createElement("h2");
     title.innerText = postTitle;
-    applyTailwindClasses(title, "text-xl font-semibold mt-4");
+    applyTailwindClasses(title, "text-xl font-semibold mt-4 line-clamp-1");
 
     const smallText = document.createElement("p");
     smallText.innerText = postBody;
-    applyTailwindClasses(smallText, "text-gray-600 mt-2 text-sm");
+    applyTailwindClasses(smallText, "text-gray-600 mt-2 text-sm line-clamp-2 border-b border-darkFaded dark:border-whiteFaded pb-2");
 
     const button = document.createElement("button");
     button.innerText = "Read more";
-    applyTailwindClasses(button, "bg-Heliotrope text-white px-4 py-2 rounded");
+    applyTailwindClasses(button, "mt-3 bg-Heliotrope text-white px-4 py-2 rounded hover:opacity-90 transition-opacity duration-200");
     button.addEventListener("click", () => {
         window.location.href = `./feed/post.html?postId=${id}`;
-
     });
 
-    card.append(postImage, title, smallText, button);
+    card.append(imageContainer, title, smallText, button);
     content.appendChild(card);
 }
+
 
 if (posts && posts.length > 0) {
     posts.forEach((post) => {
